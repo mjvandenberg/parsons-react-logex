cd669a377cb9965e3b0420dc77b784a1249968c9
mjvandenberg <martenvandenberg@hotmail.com>
2022-10-13 18:05:57 +0200

    2 files modified

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,6 +1,15 @@
+/*const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
+  list.reduce((previous, currentItem) => {
+    const group = getKey(currentItem);
+    if (!previous[group]) previous[group] = [];
+    previous[group].push(currentItem);
+    return previous;
+  }, {} as Record<K, T[]>);*/
+
 declare global {
   interface Array<T> {
     uniq(): Array<T>;
+    groupBy: <T, K extends keyof any>(item: T) => K;
   }
 }
 
@@ -8,12 +17,13 @@ Array.prototype.uniq = function () {
   return Array.from(new Set(this));
 };
 
-const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
-  list.reduce((previous, currentItem) => {
+Array.prototype.groupBy = function (getKey: (item: T) => K) {
+  return this.reduce((previous, currentItem) => {
     const group = getKey(currentItem);
     if (!previous[group]) previous[group] = [];
     previous[group].push(currentItem);
     return previous;
   }, {} as Record<K, T[]>);
+};
 
 export default global;
