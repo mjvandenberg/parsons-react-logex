2e3a2f44e7711b05b90d3c067c69f439b3f384b4
mjvandenberg <martenvandenberg@hotmail.com>
2022-10-13 18:23:01 +0200

    2 files modified

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,8 +1,6 @@
 declare global {
   interface Array<T> {
     uniq(): Array<T>;
-    //groupBy: <T, K extends keyof any>(item: T) => K;
-    //groupBy<T, K extends keyof any>(func: (item: T) => K): Record<K, T[]>;
   }
 }
 
@@ -10,21 +8,4 @@ Array.prototype.uniq = function () {
   return Array.from(new Set(this));
 };
 
-/*const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
-  list.reduce((previous, currentItem) => {
-    const group = getKey(currentItem);
-    if (!previous[group]) previous[group] = [];
-    previous[group].push(currentItem);
-    return previous;
-  }, {} as Record<K, T[]>);*/
-
-/*Array.prototype.groupBy = function (getKey: (item: T) => K) {
-  return this.reduce((previous, currentItem) => {
-    const group = getKey(currentItem);
-    if (!previous[group]) previous[group] = [];
-    previous[group].push(currentItem);
-    return previous;
-  }, {} as Record<K, T[]>);
-};*/
-
-//export default global;
+export default global;
