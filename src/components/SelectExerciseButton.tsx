934d2ee9e99071e5036fe6dafc012f12787ae203
mjvandenberg <martenvandenberg@hotmail.com>
2022-11-03 15:33:46 +0100

    1 file modified

--- src/components/SelectExerciseButton.tsx
+++ src/components/SelectExerciseButton.tsx
@@ -3,14 +3,11 @@ import { exerciseNames } from '../logEx/data/equivalence/all';
 import CaretDownIcon from './CaretDownIcon';
 
 type Props = {
-  allExercises: any;
+  exercises: any;
   onExerciseChange: (name: exerciseNames) => void;
 };
 
-const SelectExerciseButton: FC<Props> = ({
-  allExercises,
-  onExerciseChange,
-}) => {
+const SelectExerciseButton: FC<Props> = ({ exercises, onExerciseChange }) => {
   return (
     <div className="dropdown dropdown-end">
       <label tabIndex={0} className="btn btn-primary normal-case mx-1">
@@ -21,7 +18,7 @@ const SelectExerciseButton: FC<Props> = ({
         tabIndex={0}
         className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
       >
-        {Object.keys(allExercises).map((i, x) => (
+        {Object.keys(exercises).map((i, x) => (
           <li key={`${x}`}>
             <a onClick={() => onExerciseChange(i as exerciseNames)}>{i}</a>
           </li>
