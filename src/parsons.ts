c441ce3c1e9028c024c0129b9f0cc24ee55d43e7
mjvandenberg <martenvandenberg@hotmail.com>
2022-11-24 11:13:34 +0100

    1 file modified

--- src/parsons.ts
+++ src/parsons.ts
@@ -1,5 +1,17 @@
 import { ParsonsItem, ParsonsSolutionItem } from './types';
 
+interface AddToArrayFn<Type> {
+  (arr: Type[], item: Type): Type[];
+}
+
+function addArrayToEnd<T>(arr: T[], item: T) {
+  return [...arr, item];
+}
+
+function addArrayToStart<T>(arr: T[], item: T) {
+  return [item, ...arr];
+}
+
 /**
  * Validates if a parsons problem solution is valid according to the valid solution
  * Returns the solution with feedback for the student in the isValid property and
@@ -9,27 +21,54 @@ export const validateParsonsProblem: (
   solutionToValidate: ParsonsItem[],
   validSolution: ParsonsSolutionItem[]
 ) => [ParsonsItem[], boolean] = (solutionToValidate, validSolution) => {
-  const list = solutionToValidate.reduce<ParsonsItem[]>(
-    (accumulator, currentValue, currentIndex) => [
-      ...accumulator,
-      {
-        ...currentValue,
-        isValid:
-          currentIndex === 0 ||
-          (currentIndex < validSolution.length &&
-            accumulator[accumulator.length - 1].isValid === true)
-            ? validSolution[currentIndex].text === currentValue.text &&
-              validSolution[currentIndex].rule === currentValue.rule
-            : accumulator[accumulator.length - 1].isValid === true
-            ? false
-            : undefined,
-      },
-    ],
-    []
-  );
+  //reduce -> -1 or reduceRight -> +1
+  type reducers = 'reduce' | 'reduceRight';
+  type reducerOrder = {
+    type: reducers;
+    previousItem: number;
+    firstItemIndex: number;
+    addToArray: AddToArrayFn<ParsonsItem>;
+  };
+  const orders: reducerOrder[] = [
+    {
+      type: 'reduce',
+      previousItem: -1,
+      firstItemIndex: 0,
+      addToArray: addArrayToEnd,
+    },
+    {
+      type: 'reduceRight',
+      previousItem: 1,
+      firstItemIndex: solutionToValidate.length - 1,
+      addToArray: addArrayToStart,
+    },
+  ];
+
+  const reducer: (reduceOrder: reducerOrder) => ParsonsItem[] = ({
+    type,
+    firstItemIndex,
+    previousItem,
+    addToArray,
+  }) =>
+    solutionToValidate[type]<ParsonsItem[]>(
+      (accumulator, currentValue, currentIndex) =>
+        addToArray(accumulator, {
+          ...currentValue,
+          isValid:
+            currentIndex === firstItemIndex ||
+            (currentIndex < validSolution.length &&
+              accumulator[accumulator.length + previousItem].isValid === true)
+              ? validSolution[currentIndex].text === currentValue.text &&
+                validSolution[currentIndex].rule === currentValue.rule
+              : accumulator[accumulator.length + previousItem].isValid === true
+              ? false
+              : undefined,
+        }),
+      []
+    );
+  const list = reducer(orders[0]);
   const isValid =
-    list.length === validSolution.length &&
-    !list.some((i) => i.isValid === undefined || i.isValid === false);
+    list.length === validSolution.length && list.every((i) => i.isValid);
 
   return [list, isValid];
 };
