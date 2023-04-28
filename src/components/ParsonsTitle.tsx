e72a6cf7111da78df9453d769cbb42dc07a9a860
Marten van den Berg <marten.vandenberg@nslogin.nl>
2023-04-28 21:16:51 +0200

    disable favicon.ico request, other info icon

--- src/components/ParsonsTitle.tsx
+++ src/components/ParsonsTitle.tsx
@@ -10,15 +10,15 @@ const ParsonsTitle: FC<Props> = ({ exerciseName, children }) => {
     <div className="basis-0 bg-[#fcfcfc] rounded border-[#b8b8b8] border-1 border p-2 m-3">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
+        fill="currentColor"
+        viewBox="0 0 512 512"
         className="stroke-current flex-shrink-0 w-6 h-6 inline-block mr-1 relative bottom-[2px]"
       >
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth="2"
-          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"
         ></path>
       </svg>
       <strong>{exerciseName}: </strong>
