5aac8118438ad886ef397b75db7fa66ec0f9ab5f
mjvandenberg <martenvandenberg@hotmail.com>
2023-04-28 20:26:11 +0200

    Stash 2023 28 04 19 54 16 (#18)
    * 4 files modified, 1 deleted
    
    * 4 files modified, 1 deleted
    
    * 2 files modified

--- src/components/ParsonsTitle.tsx
+++ src/components/ParsonsTitle.tsx
@@ -8,12 +8,19 @@ type Props = {
 const ParsonsTitle: FC<Props> = ({ exerciseName, children }) => {
   return (
     <div className="basis-0 bg-[#fcfcfc] rounded border-[#b8b8b8] border-1 border p-2 m-3">
-      <img
-        src="/src/info.svg"
-        style={{ width: '1em', height: '1em', verticalAlign: '-.125em' }}
-        className="inline-block mr-1"
-        alt="info"
-      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="stroke-current flex-shrink-0 w-6 h-6 inline-block mr-1 relative bottom-[2px]"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
       <strong>{exerciseName}: </strong>
       {children}
     </div>
