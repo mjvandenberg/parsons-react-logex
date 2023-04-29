ea2cce7d8e563b777dedae1da598161f398eae28
mjvandenberg <martenvandenberg@hotmail.com>
2023-04-29 10:27:02 +0200

    4 files modified, 1 added

---
+++ src/components/InfoIcon.tsx
@@ -0,0 +1,21 @@
+const InfoIcon = () => {
+  return (
+    <svg
+      xmlSpace="preserve"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      fill="currentColor"
+      viewBox="0 0 512 512"
+      className="stroke-current w-5 h-5 flex-shrink-0 inline-block mr-1 relative bottom-[2px]"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"
+      ></path>
+    </svg>
+  );
+};
+
+export default InfoIcon;
